a60b6f364762dc638dea078494f3e2aea9760447
thethiago27 <thethiago27@gmail.com>
2025-06-24 17:03:09 -0400

    fix types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-export * from './routes'
-export * from './plugin'
+export {
+  Get,
+  Delete,
+  Head,
+  Put,
+  getRoutes,
+  Route,
+  HTTP_METHODS,
+  getRequestParams,
+  getResponseParams,
+  Request,
+  Response,
+  Options,
+  Post,
+  Patch
+} from './routes'
+
+export type { HttpMethod, RouteMetadata } from './routes'
+
+export {AfterRequest, getAfterRequestHooks, getBeforeRequestHooks, BeforeRequest, getPluginMetadata, isPlugin, Plugin } from './plugin'
+export type { PluginMetadata, HookMetadata } from './plugin'
